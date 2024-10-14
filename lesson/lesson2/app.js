b469a9e8c755ca5b2982a180a42e0deff0389ed8
islam <dujsobaevislam01@gmail.com>
2024-10-14 18:29:58 +0600

    lesson - 2

---
+++ lesson/lesson2/app.js
@@ -0,0 +1,14 @@
+// const text = ['my ','hello ','is ','mame ','islam ']
+//
+// console.log(text[1]+text[0]+text[3]+text[2]+text[4])
+// const result = 4 ** 2 % 10;
+// console.log(result)
+//
+//
+//
+// const MyNAME = "islam"
+// alert('my name is '+MyNAME.length+'tamgradan turat')
+const year = 2024;
+const personak = prompt('Введите ваш год рождения:');
+const age = year - personak;
+alert('Возраст: ' + age);
