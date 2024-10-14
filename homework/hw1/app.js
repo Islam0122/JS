f0837aa5b63d1d2eb386ae5b2b5ae12abd5a4538
islam <dujsobaevislam01@gmail.com>
2024-10-14 18:27:15 +0600

    hw-1

---
+++ homework/hw1/app.js
@@ -0,0 +1,18 @@
+const movie_title = "Интерстеллар";
+let release_year = 2014;
+let genres = ["Драма", "Фантастика", "Приключения"];
+let age_rating = "16+";
+let description = "Когда засуха, пыльные бури и вымирание растений приводят человечество к продовольственному кризису, коллектив исследователей и учёных отправляется в космос...";
+let box_office = null;  // не указаны данные о кассовых сборах
+let director;
+
+// Вывод значений переменных
+console.log("Название фильма:", movie_title);
+console.log("Год выпуска:", release_year);
+console.log("Жанры:", genres.join(", "));
+console.log("Возрастной рейтинг:", age_rating);
+console.log("Описание:", description);
+console.log("Кассовые сборы:", box_office);
+console.log("Режиссёр:", director);
+
+///////////  ////////////
