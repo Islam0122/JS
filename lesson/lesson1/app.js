6dc47af23fc1b70b2b40e4f4290762c55d380771
islam <dujsobaevislam01@gmail.com>
2024-10-14 18:23:11 +0600

    lesson 1

---
+++ lesson/lesson1/app.js
@@ -0,0 +1,29 @@
+// alert()
+// confirm("") true/false
+// prompt()
+// Типы данных в JS их 8 шт
+// number,string,bolean,null,underfined,object,bigint,symbol
+// var je const je let
+
+let username = "islam"
+let age = 17
+let is_married = false
+let has_car = null
+let phone_number;
+
+phone_number= 996995121007
+
+// document.write(` <h1>${username}</h1>`)
+// document.write(` <h4>${age}</h4>`)
+// document.write(` <h5>${is_married}</h5>`)
+// document.write(` <h3 style="background-color: red">${has_car}</h3>`)
+// document.write(` <h2> phone number :${phone_number}</h2>`)
+
+// операторы
+/*
+
+Это многострочный комментарий.
+
+Он может занимать несколько строк кода.
+
+*/
