a1767f2f52973033a84cc7a1ac04bb461ccbd65c
islam <dujsobaevislam01@gmail.com>
2024-10-16 02:56:22 +0600

    lesson-4

---
+++ lesson/lesson4/app.js
@@ -0,0 +1,24 @@
+const categories = ['All category', 'Dinner', 'Lunch', 'Dessert', 'Drink'];
+
+// DOM
+const div = document.querySelector('.category-btns');
+let index = 0;
+
+for (const category of categories) {
+    div.innerHTML += `
+    <button class="${categories[index] === category ? 'active' : ''}">
+      ${category}
+    </button>
+  `;
+}
+
+let buttons = div.querySelectorAll('button');
+
+for (let i = 0; i < categories.length; i++) {
+    buttons[i].onclick = function () {
+        for (const btn of buttons) {
+            btn.className = '';
+        }
+        buttons[i].className = 'active';
+    };
+}
